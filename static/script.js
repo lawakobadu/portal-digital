9b6f580516b16d9dfb4117ae6ce66982373a8614
lawakobadu <73542361+lawakobadu@users.noreply.github.com>
2023-11-20 06:48:35 +0700

    first commit

--- static/script.js
+++ static/script.js
@@ -1,15 +1,44 @@
-let video = document.getElementById("video")
+let video1 = document.getElementById("video1");
+let video2 = document.getElementById("video2");
 
 const startStream = () => {
-    navigator.mediaDevices.getUserMedia({
-        video : {
-            width: 600,
-            height: 450
-        },
-        audio : false
-    }).then((stream) => {
-        video.srcObject = stream
-    })
-}
+    navigator.mediaDevices.enumerateDevices()
+        .then(devices => {
+            const videoDevices = devices.filter(device => device.kind === 'videoinput');
 
-startStream()
+            if (videoDevices.length >= 2) {
+                navigator.mediaDevices.getUserMedia({
+                    video: {
+                        deviceId: { exact: videoDevices[0].deviceId },
+                        width: 600,
+                        height: 450
+                    },
+                    audio: false
+                }).then((stream1) => {
+                    video1.srcObject = stream1;
+                }).catch((error) => {
+                    console.error("Error accessing media devices:", error);
+                });
+
+                navigator.mediaDevices.getUserMedia({
+                    video: {
+                        deviceId: { exact: videoDevices[1].deviceId },
+                        width: 600,
+                        height: 450
+                    },
+                    audio: false
+                }).then((stream2) => {
+                    video2.srcObject = stream2;
+                }).catch((error) => {
+                    console.error("Error accessing media devices:", error);
+                });
+            } else {
+                console.error("Not enough video devices available.");
+            }
+        })
+        .catch(error => {
+            console.error("Error enumerating devices:", error);
+        });
+};
+
+startStream();
